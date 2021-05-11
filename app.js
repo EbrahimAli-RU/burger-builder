096cee95ac106600a73c2db81785894d3d33f8c0
Ebrahim Ali <ebrahimali.cse.ru@gmail.com>
2021-05-12 05:31:07 +0600

    send index.html

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const morgan = require('morgan')
 const cors = require('cors')
+const path = require('path')
 
 const authRouter = require('./router/user')
 const ingredientRouter = require('./router/ingredient')
@@ -19,13 +20,19 @@ if (process.env.NODE_ENV === "development") {
     app.use(morgan('dev'))
 }
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-}
+
 
 app.use('/api/v1/user', authRouter)
 app.use('/api/v1/ingredient', ingredientRouter)
 app.use('/api/v1/order', orderRouter)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    })
+}
 
 
 app.all(`*`, (req, res, next) => {
