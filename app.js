a8a83d018a2097ee38ba696e7ef2086f87ed529e
Ebrahim Ali <ebrahimali.cse.ru@gmail.com>
2021-05-12 05:57:15 +0600

    it says node version is not specfied

--- app.js
+++ app.js
@@ -27,11 +27,12 @@ app.use('/api/v1/ingredient', ingredientRouter)
 app.use('/api/v1/order', orderRouter)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-    })
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    });
+
 }
 
 
