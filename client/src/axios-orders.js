70522fbcac2519ca2e94aff2bd4954c11f02d0b1
Ebrahim Ali <ebrahimali.cse.ru@gmail.com>
2021-05-11 15:04:52 +0600

    Set baseUrl

--- client/src/axios-orders.js
+++ client/src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://react-my-burger-84bca.firebaseio.com/",
+  baseURL: "http://localhost:8000/api/v1",
 });
 
 export default instance;
